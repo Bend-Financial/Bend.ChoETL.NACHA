cb9a9dd098cf78f03a1fffabebd2668221a9ee1c
Nicholas Poje <npoje@websterbank.com>
2024-04-11 10:53:19 -0400

    BEND-18491 csproj and assembly changes to build/publish nuget packages

--- ChoETL.NACHA/Properties/AssemblyInfo.cs
+++ ChoETL.NACHA/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChoETL.NACHA")]
-[assembly: AssemblyDescription(".NET NACHA library")]
+[assembly: AssemblyTitle("Bend.ChoETL.NACHA")]
+[assembly: AssemblyDescription("Bend fork of .NET NACHA library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cinchoo Inc.")]
-[assembly: AssemblyProduct("ChoETL.NACHA")]
-[assembly: AssemblyCopyright("Copyright © Cinchoo Inc 2017")]
+[assembly: AssemblyCompany("Bend")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.3")]
-[assembly: AssemblyFileVersion("1.0.2.3")]
+[assembly: AssemblyVersion("1.0.2.7")]
+[assembly: AssemblyFileVersion("1.0.2.7")]
